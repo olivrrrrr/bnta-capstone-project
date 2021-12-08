7d521e63d8396a0258c91248a594c34a30f2a3e8
Rashid-F-Walcott <Rashidwalcott@gmail.com>
2021-12-08 17:22:47 +0000

    edits to User package ( unfinished) Controller, Service & repo

--- server/src/main/java/com/superleague/server/users/UserRepository.java
+++ server/src/main/java/com/superleague/server/users/UserRepository.java
@@ -1,4 +1,40 @@
 package com.superleague.server.users;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository ("Users")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+//    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+//    Optional<Student> findStudentByEmail(String email);
+//
+//    @Query("SELECT s FROM Student s WHERE s.id = ?1")
+//    Optional<Student> findStudentByID(Long StudentId);
+
+    Optional<User> getUserById(Long id);
+
+    Optional<User> getUserByEmail(String email);
+
+    void addUser(User user);
+
+    void updateUser(Long id, User user);
+
+    @Query("DELETE FROM users WHERE id = ?;")
+    Optional<User> deleteUser(Long id);
+
+    int getWeeklyPointsById(Long id);
+
+    int getWeeklyPointsByTeam(String teamName);
+
+
+
 }
+    // User methods/logic
+//    Optional <User> findUserById
+
