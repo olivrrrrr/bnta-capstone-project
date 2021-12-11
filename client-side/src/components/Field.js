ae60d8ad14d39019103e4f9a9a76d4fa96d5b6b1
olivrrrrr <oliverekwalla@gmail.com>
2021-12-11 21:02:29 +0000

    player position and player name displayed on field using object

--- client-side/src/components/Field.js
+++ client-side/src/components/Field.js
@@ -3,19 +3,23 @@ import pitch from '../assets/pitch.png'
 
 function Field() {
 
+
+    // useEffect(()=>{
+
+    // },[])
+
     const players = {
-        FWD: [null,null], 
-        MID: [null,null,null, null], 
+        FWD: ["Oli","Anand"], 
+        MID: ["Luke","Rashid","Yonis", null], 
         DEF: [null,null,null, null], 
-        GK: [null]
+        GK:  [null]
     }
 
     return (
-        <div>
+        <div> 
             <img src={pitch} sytle={{}}/>
-            {/* {Object.keys(players).map(position=> <p>{players[position].map(player=>{<p>hi</p>})}</p>)} */}
-            {Object.entries(players).map(([k,v])=><p style={{display: "flex", justifyContent:"space-between", height: "120px" , marginTop:"7em",width: "30%", position:"relative", top: 0, alignItems:"center",margin: "0 auto"}}>{
-                players[k].map(n => <p style={{width: "100%", textAlign:"center", position: "relative", bottom:"500px"}}>{k}</p>) 
+            {Object.entries(players).map(([key,val])=><p style={{display: "flex", justifyContent:"space-between", height: "120px" , marginTop:"7em",width: "25%", position:"relative", top: 0, alignItems:"center",margin: "0 auto"}}>{
+                players[key].map(n => <p style={{width: "100%", textAlign:"center", position: "relative", bottom:"500px"}}>{key}: {n}</p>) 
             }</p>)}
         </div>
     )
