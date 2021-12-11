7500de52a0c86deb4abde61cbe47e0ea777ce9a5
olivrrrrr <oliverekwalla@gmail.com>
2021-12-11 20:41:00 +0000

    css of players on the field completed

--- client-side/src/components/Field.js
+++ client-side/src/components/Field.js
@@ -11,11 +11,11 @@ function Field() {
     }
 
     return (
-        <div  >
+        <div>
             <img src={pitch} sytle={{}}/>
             {/* {Object.keys(players).map(position=> <p>{players[position].map(player=>{<p>hi</p>})}</p>)} */}
-            {Object.entries(players).map(([k,v])=><p style={{display: "flex", justifyContent:"space-between", marginTop:"60px",width: "50%", position:"relative", top: 0, alignItems:"center",margin: "0 auto"}}>{
-                players[k].map(n => <p style={{width: "100%", textAlign:"center", position: "relative", bottom:"500px"}}> player</p>) 
+            {Object.entries(players).map(([k,v])=><p style={{display: "flex", justifyContent:"space-between", height: "120px" , marginTop:"7em",width: "30%", position:"relative", top: 0, alignItems:"center",margin: "0 auto"}}>{
+                players[k].map(n => <p style={{width: "100%", textAlign:"center", position: "relative", bottom:"500px"}}>{k}</p>) 
             }</p>)}
         </div>
     )
