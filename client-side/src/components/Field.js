6d40707204fd72ab9460ad513887cd2e7c1b17c3
olivrrrrr <oliverekwalla@gmail.com>
2021-12-11 23:38:29 +0000

    useEffect added to extract player data

--- client-side/src/components/Field.js
+++ client-side/src/components/Field.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import pitch from '../assets/pitch.png'
+import {React,useState,useEffect} from 'react'
+import field from '../assets/pitch.png'
 
 function Field() {
 
+    const [players, setPlayers] = useState([]);
 
-    // useEffect(()=>{
+    useEffect(()=>{
+        fetch("http://localhost:8080/api/v1/players/allPlayer")
+        .then(resp=>resp)
+        .then(data=>setPlayers(data))
+    },[])
 
-    // },[])
+    console.log(players)
 
-    const players = {
+    const pitch = {
         FWD: ["Oli","Anand"], 
-        MID: ["Luke","Rashid","Yonis", null], 
+        MID: ["Luke","Rashid","Yonis", "Ronaldo"], 
         DEF: [null,null,null, null], 
         GK:  [null]
     }
 
     return (
         <div> 
-            <img src={pitch} sytle={{}}/>
-            {Object.entries(players).map(([key,val])=><p style={{display: "flex", justifyContent:"space-between", height: "120px" , marginTop:"7em",width: "25%", position:"relative", top: 0, alignItems:"center",margin: "0 auto"}}>{
-                players[key].map(n => <p style={{width: "100%", textAlign:"center", position: "relative", bottom:"500px"}}>{key}: {n}</p>) 
-            }</p>)}
+            <img src={field} sytle={{}}/>
+            {Object.entries(pitch).map(([key,val])=><div key={val} style={{display: "flex", justifyContent:"space-between", height: "120px" , marginTop:"7em",width: "25%", position:"relative", top: 0, alignItems:"center",margin: "0 auto"}}>{
+                pitch[key].map((player,i) => <p key={i} style={{width: "100%", textAlign:"center", position: "relative", bottom:"500px"}}>{key}: {player}</p>) 
+            }</div>)}
         </div>
     )
 }
