8e9a8bbdfdaa39a2ae048e43d9d54f1880bdf892
anandlakhani9 <58656737+anandlakhani9@users.noreply.github.com>
2021-12-14 10:41:33 +0000

    fetch moved to button click

--- client-side/src/pages/AdminPage.js
+++ client-side/src/pages/AdminPage.js
@@ -8,26 +8,26 @@ function AdminPage() {
 
     const [players, setPlayers] = useState([]);
     // prem, bunds, seriaa, ligue1, laliga
-    const leagues = [39, 78, 135, 61, 140]
-    const pages = [36, 33, 44, 40, 35]
+    // const leagues = [39, 78, 135, 61, 140]
+    // const pages = [36, 33, 44, 40, 35]
 
-    useEffect(() => {
-        // getAllPlayersPrem();
-        // getAllPlayersBundesliga();
-        for (let i = 0; i <= leagues.length; i++) {
-            const league = leagues[i]
-            const page = pages[i]
-            for (let i = 1; i <= page; i++) {
-                fetchPlayerData(league, i)
-                    .then(response => {
-                        // console.log(response);
-                        setPlayers(players => players.concat(response));
-                        console.log(players)
-                        return 
-                    })
-            }
-        } return 
-    }, [])
+    // useEffect(() => {
+    //     getAllPlayersPrem();
+    //     getAllPlayersBundesliga();
+    //     for (let i = 0; i <= leagues.length; i++) {
+    //         const league = leagues[i]
+    //         const page = pages[i]
+    //         for (let i = 1; i <= page; i++) {
+    //             fetchPlayerData(league, i)
+    //                 .then(response => {
+    //                     // console.log(response);
+    //                     setPlayers(players => players.concat(response));
+    //                     console.log(players)
+    //                     return 
+    //                 })
+    //         }
+    //     } return 
+    // }, [])
 
     useEffect(() => console.log(players), [players])
 
@@ -45,46 +45,65 @@ function AdminPage() {
         updateAllPlayers(players)
     }
 
-    const getAllFromLeague = (theLeague, noPages) => {
-        const league = theLeague
-        const pages = noPages
-        for (let i = 1; i <= pages; i++) {
-            fetchPlayerData(league, i)
-                .then(response => {
-                    // console.log(response);
-                    setPlayers(players.concat(response));
-                    console.log(players)
-                    return players
-                })
+    // const getAllFromLeague = (theLeague, noPages) => {
+    //     const league = theLeague
+    //     const pages = noPages
+    //     for (let i = 1; i <= pages; i++) {
+    //         fetchPlayerData(league, i)
+    //             .then(response => {
+    //                 // console.log(response);
+    //                 setPlayers(players => players.concat(response));
+    //                 //console.log(players)
+    //                 return players
+    //             })
+    //     }
+    // }
+
+    const getAllFromLeague = () => {
+        // prem, bunds, seriaa, ligue1, laliga
+        const leagues = [39, 78, 135, 61, 140]
+        const pages = [36, 33, 44, 40, 35]
+        for (let i = 0; i <= leagues.length; i++) {
+            const league = leagues[i]
+            const page = pages[i]
+            for (let i = 1; i <= page; i++) {
+                fetchPlayerData(league, i)
+                    .then(response => {
+                        // console.log(response);
+                        setPlayers(players => players.concat(response));
+                        //console.log(players)
+                        return 
+                    })
+            }
         }
     }
 
-    const getAllPlayersPrem = () => {
-        const league = 39
-        const pages = 3
-        for (let i = 1; i <= pages; i++) {
-            fetchPlayerData(league, i)
-                .then(response => {
-                    setPlayers(players.concat(response))
-                    return players
-                })
-        }
-    }
+    // const getAllPlayersPrem = () => {
+    //     const league = 39
+    //     const pages = 3
+    //     for (let i = 1; i <= pages; i++) {
+    //         fetchPlayerData(league, i)
+    //             .then(response => {
+    //                 setPlayers(players.concat(response))
+    //                 return players
+    //             })
+    //     }
+    // }
 
-    // console.log(players);
+    // // console.log(players);
 
 
-    const getAllPlayersBundesliga = () => {
-        const league = 78
-        const pages = 3
-        for (let i = 1; i <= pages; i++) {
-            fetchPlayerData(league, i)
-                .then(response => {
-                    setPlayers(players.concat(response))
-                    return players
-                })
-        }
-    }
+    // const getAllPlayersBundesliga = () => {
+    //     const league = 78
+    //     const pages = 3
+    //     for (let i = 1; i <= pages; i++) {
+    //         fetchPlayerData(league, i)
+    //             .then(response => {
+    //                 setPlayers(players.concat(response))
+    //                 return players
+    //             })
+    //     }
+    // }
 
 
     // const getAllPlayersPrem = () => {
@@ -103,15 +122,20 @@ function AdminPage() {
 
         players.length > 0 ?
 
-
             <div>
+                <Button clickMethod={getAllFromLeague} name="fetch all player data" />
                 <Button clickMethod={postPlayersToDb} name="Post players to db" />
-                <Button clickMethod={updateAllPlayers} name="Update player stats" />
+                <Button clickMethod={updatePlayersInDb} name="Update player stats" />
                 {players[0].name}
             </div>
 
             :
-            <div>loading...</div>
+            // <div>loading...</div>
+            <div>
+                <Button clickMethod={getAllFromLeague} name="fetch all player data" />
+                <Button clickMethod={postPlayersToDb} name="Post players to db" />
+                <Button clickMethod={updatePlayersInDb} name="Update player stats" />
+            </div>
     )
 }
 
